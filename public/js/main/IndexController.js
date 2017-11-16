0d6f0206a07f10df3842c9fab7305e0988617f76
Benny Powers <benny.p@elasticmedia.io>
2017-11-16 22:54:59 +0200

    Refactor ES5 style Constructors to ES6 class keyword constructors

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -2,114 +2,94 @@ import PostsView from './views/Posts';
 import ToastsView from './views/Toasts';
 import idb from 'idb';
 
-export default function IndexController(container) {
-  this._container = container;
-  this._postsView = new PostsView(this._container);
-  this._toastsView = new ToastsView(this._container);
-  this._lostConnectionToast = null;
-  this._openSocket();
-  this._registerServiceWorker();
-}
+export default class IndexController {
 
-IndexController.prototype._registerServiceWorker = function() {
-  if (!navigator.serviceWorker) return;
-
-  var indexController = this;
-
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
-    if (!navigator.serviceWorker.controller) {
-      return;
-    }
-
-    if (reg.waiting) {
-      indexController._updateReady(reg.waiting);
-      return;
-    }
-
-    if (reg.installing) {
-      indexController._trackInstalling(reg.installing);
-      return;
-    }
-
-    reg.addEventListener('updatefound', function() {
-      indexController._trackInstalling(reg.installing);
-    });
-  });
-
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    navigator.serviceWorker.getRegistrations()
-      .then(([reg]) => reg.unregister())
-      .all(() => window.location.reload());
-  });
-};
-
-IndexController.prototype._trackInstalling = function(worker) {
-  var indexController = this;
-  worker.addEventListener('statechange', function() {
-    if (worker.state == 'installed') {
-      indexController._updateReady(worker);
-    }
-  });
-};
-
-IndexController.prototype._updateReady = function(worker) {
-  var toast = this._toastsView.show("New version available", {
-    buttons: ['refresh', 'dismiss']
-  });
-
-  toast.answer.then(function(answer) {
-    if (answer != 'refresh' || worker.state === 'redundant') return;
-    worker.postMessage({answer});
-  });
-};
-
-// open a connection to the server for live updates
-IndexController.prototype._openSocket = function() {
-  var indexController = this;
-  var latestPostDate = this._postsView.getLatestPostDate();
-
-  // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
-
-  if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
+  constructor(container) {
+    this._container = container;
+    this._postsView = new PostsView(this._container);
+    this._toastsView = new ToastsView(this._container);
+    this._lostConnectionToast = null;
+    this._openSocket();
+    this._registerServiceWorker();
   }
 
-  // this is a little hack for the settings page's tests,
-  // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
+  async _registerServiceWorker() {
+    if (!navigator.serviceWorker) return;
 
-  var ws = new WebSocket(socketUrl.href);
+    const reg = navigator.serviceWorker.register('/sw.js');
 
-  // add listeners
-  ws.addEventListener('open', function() {
-    if (indexController._lostConnectionToast) {
-      indexController._lostConnectionToast.hide();
+    if (!navigator.serviceWorker.controller) return;
+
+    if (reg.waiting) return this._updateReady(reg.waiting);
+
+    if (reg.installing) return this._trackInstalling(reg.installing);
+
+    reg.onupdatefound = () => this._trackInstalling(reg.installing);
+
+    navigator.serviceWorker.oncontrollerchange = async () => {
+      const [reg] = navigator.serviceWorker.getRegistrations();
+      reg.unregister();
+      window.location.reload();
+    };
+  }
+
+  _trackInstalling(worker) {
+    worker.onstatechange = () => worker.state == 'installed' && this._updateReady(worker);
+  }
+
+  async _updateReady(worker) {
+    const buttons = ['refresh', 'dismiss'];
+    const toast = this._toastsView.show("New version available", {buttons});
+
+    const answer = await toast.answer();
+    return (answer !== 'refresh' || worker.state === 'redundant') || worker.postMessage({answer});
+  }
+
+  // open a connection to the server for live updates
+  _openSocket() {
+    const latestPostDate = this._postsView.getLatestPostDate();
+
+    // create a url pointing to /updates with the ws protocol
+    const socketUrl = new URL('/updates', window.location);
+    socketUrl.protocol = 'ws';
+
+    if (latestPostDate) {
+      socketUrl.search = `since=${latestPostDate.valueOf()}`;
     }
-  });
 
-  ws.addEventListener('message', function(event) {
-    requestAnimationFrame(function() {
-      indexController._onSocketMessage(event.data);
+    // this is a little hack for the settings page's tests,
+    // it isn't needed for Wittr
+    socketUrl.search += '&' + location.search.slice(1);
+
+    const ws = new WebSocket(socketUrl.href);
+
+    // add listeners
+    ws.addEventListener('open', () => {
+      if (this._lostConnectionToast) {
+        this._lostConnectionToast.hide();
+      }
     });
-  });
 
-  ws.addEventListener('close', function() {
-    // tell the user
-    if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
-    }
+    ws.addEventListener('message', (event) => {
+      requestAnimationFrame(() => {
+        this._onSocketMessage(event.data);
+      });
+    });
 
-    // try and reconnect in 5 seconds
-    setTimeout(function() {
-      indexController._openSocket();
-    }, 5000);
-  });
-};
+    ws.addEventListener('close', () => {
+      // tell the user
+      if (!this._lostConnectionToast) {
+        this._lostConnectionToast = this._toastsView.show("Unable to connect. Retrying…");
+      }
 
-// called when the web socket sends message data
-IndexController.prototype._onSocketMessage = function(data) {
-  var messages = JSON.parse(data);
-  this._postsView.addPosts(messages);
-};
+      // try and reconnect in 5 seconds
+      setTimeout(() => this._openSocket(), 5000);
+    });
+  }
+
+  // called when the web socket sends message data
+  _onSocketMessage(data) {
+    const messages = JSON.parse(data);
+    this._postsView.addPosts(messages);
+  }
+}
--- public/js/main/views/Toasts.js
+++ public/js/main/views/Toasts.js
@@ -4,73 +4,70 @@ import defaults from 'lodash/object/defaults';
 import transition from 'simple-transition';
 import closest from 'closest';
 
-function Toast(text, duration, buttons) {
-  var toast = this;
+class Toast {
+  constructor(text, duration, buttons) {
+    this.container = parseHTML(toastTemplate({text, buttons})).firstChild;
 
-  this.container = parseHTML(toastTemplate({
-    text: text,
-    buttons: buttons
-  })).firstChild;
+    this.answer = new Promise(function(resolve) {
+      this._answerResolver = resolve;
+    });
 
-  this.answer = new Promise(function(resolve) {
-    toast._answerResolver = resolve;
-  });
+    this.gone = new Promise(function(resolve) {
+      this._goneResolver = resolve;
+    });
 
-  this.gone = new Promise(function(resolve) {
-    toast._goneResolver = resolve;
-  });
+    if (duration) {
+      this._hideTimeout = setTimeout(this.hide, duration);
+    }
 
-  if (duration) {
-    this._hideTimeout = setTimeout(function() {
-      toast.hide();
-    }, duration);
+    this.container.addEventListener('click', (event) => {
+      const button = closest(event.target, 'button', true);
+      if (!button) return;
+      this._answerResolver(button.textContent);
+      this.hide();
+    });
   }
 
-  this.container.addEventListener('click', function(event) {
-    var button = closest(event.target, 'button', true);
-    if (!button) return;
-    toast._answerResolver(button.textContent);
-    toast.hide();
-  });
+  hide() {
+    clearTimeout(this._hideTimeout);
+    this._answerResolver();
+
+    transition(this.container, {
+      opacity: 0
+    }, 0.3, 'ease-out').then(this._goneResolver);
+
+    return this.gone;
+  }
 }
 
-Toast.prototype.hide = function() {
-  clearTimeout(this._hideTimeout);
-  this._answerResolver();
+export default class Toasts {
 
-  transition(this.container, {
-    opacity: 0
-  }, 0.3, 'ease-out').then(this._goneResolver);
-  
-  return this.gone;
-};
+  constructor(appendToEl) {
+    this._container = parseHTML('<div class="toasts"></div>').firstChild;
+    appendToEl.appendChild(this._container);
+  }
 
-export default function Toasts(appendToEl) {
-  this._container = parseHTML('<div class="toasts"></div>').firstChild;
-  appendToEl.appendChild(this._container);
+  // show a message to the user eg:
+  // toasts.show("Do you wish to continue?", {
+  //   buttons: ['yes', 'no']
+  // })
+  // Returns a toast.
+  show(message, opts) {
+    const duration = 0;
+    const buttons = ['dismiss'];
+    opts = defaults({}, opts, {duration, buttons});
+
+    const toast = new Toast(message, opts.duration, opts.buttons);
+    this._container.appendChild(toast.container);
+
+    transition(toast.container, {
+      opacity: 1
+    }, 0.5, 'ease-out');
+
+    toast.gone.then(function() {
+      toast.container.parentNode.removeChild(toast.container);
+    });
+
+    return toast;
+  }
 }
-
-// show a message to the user eg:
-// toasts.show("Do you wish to continue?", {
-//   buttons: ['yes', 'no']
-// })
-// Returns a toast.
-Toasts.prototype.show = function(message, opts) {
-  opts = defaults({}, opts, {
-    duration: 0,
-    buttons: ['dismiss']
-  });
-
-  var toast = new Toast(message, opts.duration, opts.buttons);
-  this._container.appendChild(toast.container);
-
-  transition(toast.container, {
-    opacity: 1
-  }, 0.5, 'ease-out');
-
-  toast.gone.then(function() {
-    toast.container.parentNode.removeChild(toast.container);
-  });
-
-  return toast;
-};
